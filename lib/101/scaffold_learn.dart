65ef224d1a189eb8cfc7f231cab62005bc7576d7
suleymanakca <sakca3253@gmail.com>
2022-10-18 18:55:38 +0300

    second commit

--- lib/101/scaffold_learn.dart
+++ lib/101/scaffold_learn.dart
@@ -10,7 +10,7 @@ class ScaffoldLearnView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Scaffold Samples'),
       ),
-      body: const Text('merhaba'),
+      body: const Text('Hello'),
       backgroundColor: Colors.red,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
